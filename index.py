9a56b82ad245ef6bc72942ec035eed98e7339f05
bruce851117 <63329650+bruce851117@users.noreply.github.com>
2020-06-16 00:31:40 +0800

    add keyword selection

--- index.py
+++ index.py
@@ -29,15 +29,16 @@ def login():
         default_year = '05/06/2020'
         selected = {'pph_1':'selected','pph_2':'','pph_3':'','pph_4':''}
         default_method = 'pph_1'
-        key1,top_news_1 = get_top_news(default_year, 1)
-        key2,top_news_2 = get_top_news(default_year, 2)
-        key3,top_news_3 = get_top_news(default_year, 3)
+        key1,top_news_1 = get_top_news(default_year, 1,'')
+        key2,top_news_2 = get_top_news(default_year, 2,'')
+        key3,top_news_3 = get_top_news(default_year, 3,'')
         portfolio_list,portfolio_news = get_portfolio_news(default_year,default_method)
         return render_template("main.html",
                                date = default_year,
                                selected = selected,
                                portfolio = portfolio_list,
                                portfolio_news = portfolio_news,
+                               keyword = '',
                                key1 = key1,
                                top_news_list_1 = top_news_1,
                                key2 = key2,
@@ -52,7 +53,7 @@ def op():
     if request.method == "POST":
         year = request.values['datepicker']
         portfolio = request.values['portfolio']
-        keyword = request.values['ikeyword']
+        keyword = request.form['ikeyword']
         with open(f'./schema/Info.json', 'r') as json_file:
            data = json.load(json_file)
         int = 0
@@ -69,16 +70,16 @@ def op():
         selected = {'pph_1':'','pph_2':'','pph_3':'','pph_4':''}
         selected[portfolio]='selected'
         date = pd.to_datetime(year).strftime('%m/%d/%Y')
-        key1,top_news_1 = get_top_news(year, 1)
-        key2,top_news_2 = get_top_news(year, 2)
-        key3,top_news_3 = get_top_news(year, 3)
+        key1,top_news_1 = get_top_news(year, 1,keyword)
+        key2,top_news_2 = get_top_news(year, 2,keyword)
+        key3,top_news_3 = get_top_news(year, 3,keyword)
         portfolio_list,portfolio_news = get_portfolio_news(year,portfolio)
-
         return render_template("main.html",
                                date = date,
                                selected = selected,
                                portfolio = portfolio_list,
                                portfolio_news = portfolio_news,
+                               keyword = keyword,
                                key1 = key1,
                                top_news_list_1 = top_news_1,
                                key2 = key2,
@@ -87,13 +88,22 @@ def op():
                                top_news_list_3 = top_news_3,
                                )
 
-def get_top_news(which_day,num):
+def get_top_news(which_day,num,keyword):
     which_day = pd.to_datetime(which_day).strftime('%Y%m%d')
     with open(f'./UIData/news/{which_day}_{num}.json')as f:
         file = json.load(f)
         key = file[0]
         news = file[1:]
+    if keyword != '':
+        keyword = keyword.upper()
+        choose = []
+        for i in news:
+            title = i['title'].upper().split()
+            if keyword in title:
+                choose.append(i)
+        news = choose
     return key,news
+            
 def get_portfolio_news(which_day,method):
     which_day = pd.to_datetime(which_day).strftime('%Y%m%d')
     method = method_list[method]
@@ -111,11 +121,12 @@ def get_portfolio_news(which_day,method):
         portfolio = ['no stocks choosed by this method on this date']
         news = [{'title':'no stocks choosed by this method on this date'}]
         return portfolio,news
+    
 @app.route("/log/create-entry", methods=["POST"])
 def create_entry():
     req = request.get_json()
     print(req)
-    with open(f'Info.json', 'r') as json_file:
+    with open(f'./schema/Info.json', 'r') as json_file:
         data = json.load(json_file)
     int = 0
     try:
@@ -127,7 +138,7 @@ def create_entry():
                              "pf": "",
                              "kw": "",
                              "clc": {"url": req['url'], "title" : req['title'], "tab": req['tab']}}})
-    with open(f'Info.json', 'w') as json_file:
+    with open(f'./schema/Info.json', 'w') as json_file:
         json.dump(data, json_file)
         json_file.close()
     res = make_response(jsonify({"message": "OK"}), 200)
